eefd42c547ffa73e6fc26c6ca8d10d13ccab0e82
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-09-30 16:48:39 +0200

    new release on pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyfonts",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     description="A simple way to load fonts for matplotlib",
     long_description=open("README.md").read(),
@@ -10,7 +10,5 @@ setup(
     author="Joseph Barbier",
     author_email="joseph.barbierdarnal@gmail.com",
     url="https://github.com/JosephBARBIERDARNAL/pyfonts/blob/main/README.md",
-    install_requires=[
-        "matplotlib"
-    ],
+    install_requires=["matplotlib"],
 )
